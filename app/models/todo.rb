9bd315e90f7254dea44bba3b1cdd642350dd0079
vineeth-utd <vineeth.jaisankar@gmail.com>
2020-04-11 11:47:26 +0530

    Precompile assets

--- app/models/todo.rb
+++ app/models/todo.rb
@@ -1,6 +1,42 @@
 class Todo < ActiveRecord::Base
-  def to_pleasant_string
-    is_completed = completed ? "[X]" : "[ ]"
-    "#{id}. #{due_date.to_s(:long)} #{todo_text} #{is_completed}"
+  def self.overdue
+    all.where("due_date < ?", Date.today)
+  end
+
+  def self.due_today
+    all.where("due_date = ?", Date.today)
+  end
+
+  def self.due_later
+    all.where("due_date > ?", Date.today)
+  end
+
+  def due_today?
+    due_date == Date.today
+  end
+
+  def self.completed
+    all.where(completed: true)
+  end
+
+  def self.show_list
+    puts "\nMy Todolist"
+    puts "\nOverdue"
+    puts overdue.map { |todo| todo.to_displayable_string }
+    puts "\nDue Today"
+    puts due_today.map { |todo| todo.to_displayable_string }
+    puts "\nDue Later"
+    puts due_later.map { |todo| todo.to_displayable_string }
+  end
+
+  def self.add_task(todo)
+    create!(todo_text: todo[:todo_text], due_date: Date.today + todo[:due_in_days], completed: false)
+  end
+
+  def self.mark_as_complete!(todo_id)
+    todo = Todo.find(todo_id)
+    todo.completed = true
+    todo.save
+    todo
   end
 end
