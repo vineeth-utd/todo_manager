1841a7af6c349cf9365a8c76127766a6bced2de5
vineeth-utd <vineeth.jaisankar@gmail.com>
2020-04-12 20:02:42 +0530

    Overdue part done

--- db/schema.rb
+++ db/schema.rb
@@ -10,27 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_22_063959) do
+ActiveRecord::Schema.define(version: 2020_04_11_140537) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
-
-  create_table "articles", force: :cascade do |t|
-    t.string "article_name"
-    t.string "article_type"
-  end
-
-  create_table "productss", force: :cascade do |t|
-    t.integer "product_id"
-    t.string "product_name"
-    t.float "cost"
-  end
-
-  create_table "student", id: false, force: :cascade do |t|
-    t.integer "regno"
-    t.string "name", limit: 20
-    t.float "cgpa"
-  end
 
   create_table "todos", force: :cascade do |t|
     t.text "todo_text"
