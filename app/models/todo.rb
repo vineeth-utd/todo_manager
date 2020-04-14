aeb9ecb7b8b18e6f4608b91f8d667b0686da6f8f
vineeth-utd <vineeth.jaisankar@gmail.com>
2020-04-14 11:13:56 +0530

    Edited Overdue,DueToday,DueLater sections

--- app/models/todo.rb
+++ app/models/todo.rb
@@ -1,6 +1,6 @@
 class Todo < ActiveRecord::Base
   def self.overdue
-    all.where("due_date < ? and (not completed)", Date.today).ordered
+    all.where("due_date < ? and (not completed)", Date.today)
   end
 
   def self.due_today
