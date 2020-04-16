dfcc8fdf67c800c93ce513a499b212a6d0877741
vineeth-utd <vineeth.jaisankar@gmail.com>
2020-04-16 19:33:00 +0530

    Storing passwords with bcrypt and password digests

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,8 +12,8 @@ class UsersController < ApplicationController
   def create
     user = User.create!(first_name: params[:first_name],
                         last_name: params[:last_name],
-                        email: params[:email])
-    #password_digest: digest(params[:password]))
+                        email: params[:email],
+                        password: params[:password])
     redirect_to "/"
   end
 
