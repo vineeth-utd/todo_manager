859016191c12c94f9a7ceb3e045537d4f2db761e
vineeth-utd <vineeth.jaisankar@gmail.com>
2020-04-17 19:34:03 +0530

    Validate first_name and email before sign-up

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,6 @@ Rails.application.routes.draw do
   resources :todos
 
   resources :users
-  #post "users/login", to: "users#login"
 
   get "/signin" => "sessions#new", as: :new_sessions
   post "/signin" => "sessions#create", as: :sessions
