300a4355cc0c2ec71061f60a6280a433bb624b91
vineeth-utd <vineeth.jaisankar@gmail.com>
2020-04-17 12:34:51 +0530

    Implement sign-out by resetting the user section

--- config/routes.rb
+++ config/routes.rb
@@ -10,8 +10,9 @@ Rails.application.routes.draw do
   resources :todos
 
   resources :users
-  post "users/login", to: "users#login"
+  #post "users/login", to: "users#login"
 
   get "/signin" => "sessions#new", as: :new_sessions
   post "/signin" => "sessions#create", as: :sessions
+  delete "/signout" => "sessions#destroy", as: :destroy_session
 end
